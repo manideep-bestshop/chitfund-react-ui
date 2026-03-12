20caf37968412e5a4a623c1903dc95b7ea791873
Manideep Nampally <157095258+manideep-bestshop@users.noreply.github.com>
2026-03-12 16:17:21 +0530

    Update API base URL for production environment

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,3 +1,3 @@
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://localhost:7254';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://brave-sky-00bcf220f.2.azurestaticapps.net';
 
 export default API_BASE_URL;
